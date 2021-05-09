5288fefdef0885af07869b32a04190a3c6e68064
golden-arches <69598810+golden-arches@users.noreply.github.com>
2021-05-09 15:10:56 -0700

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/************************
+Code by: Micheal Gossar
+*************************/
+// int, float, string, bool, and double 
+
+void myFunction() {
+	float x = 2.30f;
+	std::cout << x + 3.00f << std::endl;
+}
+
+int main() {
+	myFunction();
+	return 0;
+}
